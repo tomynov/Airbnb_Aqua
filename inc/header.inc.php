b0cbcdf3e45538bda1880ffa3bcb11004bc02b8f
Tom <tom.cousdikian@ynov.com>
2020-06-10 11:55:23 +0200

    ajout_block

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -7,12 +7,12 @@
         <meta name="description" content="Le site Aquabnb est le tout premier site de location dédié à l'immensité de nos océans !">
         <script src="https://kit.fontawesome.com/782e7c6869.js" crossorigin="anonymous"></script>
         <title>Aquabnb</title>
-        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="css/style.css">
         <script src="../js/main.js" async> </script>
     </head>
 
     <header>
-        <section class="head_qua">
+        <section class="head_aqua">
 
             <div class="top_aqua"> <!-- Haut de page-->
                 <h1 id="title_header"> Aquabnb </h1>
--- index.php
+++ index.php
@@ -1,1 +1,44 @@
 <?php include("inc/header.inc.php"); ?>
+
+<body>
+
+    <main>
+
+        <div class="recherche_head"> <!-- Barre de Recherche-->
+            <div class="block_recherche"> 
+                <label> Lieux </label>
+                <input type="text" placeholder="Votre destination" required>
+            </div>
+            <div class="block_recherche"> 
+                <label >Arrivée </label>
+                <input type="date" placeholder="Date d'arrivé " required>
+            </div>
+            <div class="block_recherche"> 
+                <label >Départ </label>
+                <input type="date" placeholder="Date de départ " required>
+            </div>
+            <div class="block_recherche">
+                <label> Voyageurs </label> 
+                <input type="number" min="1" max="99" placeholder="Nombre de voyageurs" required>
+            </div>
+            <div class="block_recherche">
+                <a href="">
+                    <button typbe="submit"> <i class="fas fa-search"></i> Rechercher </button>
+                </a>
+            </div>
+        </div>
+
+        <h1 id="title_main"> Voyager Différemment avec AquaBnb </h1> 
+
+        <div class="block_main">
+            <h3 class="title_block_main" > Les Destinations préférées de nos Utilisateurs <i class="fas fa-heart"></i> </h3>
+        </div>
+        <div class="block_main">
+            <h3 class="title_block_main" > En manque d'inspiration ? <i class="fas fa-plane-departure"></i> </h3>
+        </div>
+        <div class="block_main">
+            <h3 class="title_block_main" > Contactez_nous <i class="fas fa-heart"></i> </h3>
+        </div>
+    </main>
+
+</body>
