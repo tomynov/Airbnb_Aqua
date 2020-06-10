d67fd4b118adde158f19f5891c34b1ea28699321
Flavio <flaviocabecas2@gmail.com>
2020-06-10 10:29:12 +0200

    première version html/css de la page profil

---
+++ profil.php
@@ -0,0 +1,36 @@
+<?php include("inc/header.inc.php"); ?>
+
+<body>
+
+    <section class="info-user"> <!--bordure-->
+        <div class="bloc-profil">
+            <h2> Vos informations </h2>
+            <div class="bloc-info">
+                <div class="img-profil"> 
+                    <!--Image-->
+                </div>
+                <div class="informations">
+                    <h3 class="h3-user"> Nom </h3>
+                    <h3 class="h3-user"> Adresse Mail </h3>
+                </div>
+            </div>
+            <div class="modif">
+                <a href="modif.php" alt="modif profile"><button class="button-modif"> Modifier </button></a>
+            </div>
+        </div>
+    </section>
+     
+    <div class="center-line">
+        <div class="line">
+            <!--trait de séparation-->
+        </div>
+    </div>
+
+    <section class="user-location">
+        <h2> Mes hébergements </h2>
+        <div class="info-bloc1">
+
+        </div>
+    </section>
+
+</body>
