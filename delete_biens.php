9fd587a77b88878d118fcc89a9b1f83c169e52e9
Flavio <flaviocabecas2@gmail.com>
2020-06-15 18:32:26 +0200

    arrangements

--- delete_biens.php
+++ delete_biens.php
@@ -8,9 +8,15 @@
 
 ?>
 
-    <div class="supp-form">
-        <p>Votre hébergement à bien été supprimé et n'est plus visible par les visiteurs d'aquabnb.</p>
-        <a href="biens.php" alt="retour à ses hebergements"><button> Retour à vos biens </button></a>
-    </div>
+    <main>
+        <div class="supp-form">
+            <p>Votre hébergement à bien été supprimé et n'est plus visible par les visiteurs d'aquabnb.</p>
+            <?php
+                $result = $pdo->query("SELECT * FROM hebergement WHERE id_hebergement = $_GET[id] ORDER BY id_hebergement DESC");
+                $retour = $pdo->exec($result);
+            ?>
+            <a href="biens.php?id=<?php echo $retour->id_user ?>" alt="retour à ses hebergements"><button> Retour à vos biens </button></a>
+        </div>
+    </main>
 
 </body>
